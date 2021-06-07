0f60bd6876dbfeb7decd372fc04108b7feeaf480
Arjun Dadhwal <arjund2002@gmail.com>
2021-06-07 21:00:28 +0530

    integrated with redux storage

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import './App.css';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { update } from './actions';
+import throttle from 'lodash/throttle'
 
 import reducers from './reducers';
+import { loadState, saveState} from './utils'
 
 import NewTimer from './components/NewTimer'
 import ListTimers from './components/ListTimers'
 
 const store = createStore(reducers);
+
+store.subscribe(throttle(() => {
+  saveState(store.getState())
+}, 1000));
 
 let lastUpdateTime = Date.now()
 setInterval(() => {
