acd2a11d5960acdd65f183ba66fd7412245ce79d
Arjun Dadhwal <arjund2002@gmail.com>
2021-06-08 17:04:05 +0530

    changed time formatting

--- src/utils/index.js
+++ src/utils/index.js
@@ -31,5 +31,5 @@ export const formatTime = (time) => {
     const secs = Math.floor(time / 1000) % 60
     const mins = Math.floor(time / 1000 / 60) % 60
     const hrs = Math.floor(time / 1000 / 1000 / 60) % 60
-    return `${hrs}:${mins}:${secs}.${ms}`
+    return `${hrs>0? hrs+"h" : ""}${mins>0? mins+"m" : ""}${secs>0? secs+"s" : "s"}`
 }
